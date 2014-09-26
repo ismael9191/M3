954814105b0a085c4ae9994052014a42bfae5606
Ismael <Ismael@Ismael-PC>
2014-09-26 16:37:01 +0200

    Commit Inicial

---
+++ src/M3/M3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M3;
+
+/**
+ *
+ * @author Ismael
+ */
+public class M3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 5;
+        
+        int resultado;
+        
+        resultado = n1+n2;
+        System.out.println("Resultado:"+ resultado);
+    }
+    
+}
